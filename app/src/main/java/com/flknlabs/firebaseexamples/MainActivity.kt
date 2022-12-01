c2af2b0fb5cfb6a6a915abb02785892598eb16a6
Gabriel Villarreal <gabrielvillarreal78@gmail.com>
2022-12-01 15:45:07 -0300

    fixing the branches

--- app/src/main/java/com/flknlabs/firebaseexamples/MainActivity.kt
+++ app/src/main/java/com/flknlabs/firebaseexamples/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.flknlabs.firebaseexamples
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import com.facebook.AccessToken
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.FirebaseException
 import com.google.firebase.FirebaseTooManyRequestsException
 import com.google.firebase.auth.*
@@ -48,7 +62,25 @@ class MainActivity : AppCompatActivity() {
             storedVerificationId = verificationId
         }
     }
+    private lateinit var googleSignInClient: GoogleSignInClient
 
+    private val callbackManager = CallbackManager.Factory.create()
+
+    private val startForResult = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()) {
+            result: ActivityResult ->
+        if (result.resultCode == Activity.RESULT_OK) {
+
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            try {
+                val account = task.getResult(ApiException::class.java)!!
+                Log.d(TAG, "firebaseAuthWithGoogle:" + account.id)
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                Log.w(TAG, "Google sign in failed", e)
+            }
+        }
+    }
     companion object {
         const val TAG = "MainActivity"
     }
@@ -85,6 +117,20 @@ class MainActivity : AppCompatActivity() {
         btnValidSMS.setOnClickListener { validSMS(
             edtCode.text.toString()
         ) }
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+        btnLoginGoogle.setOnClickListener { loginGoogle() }
+
+
+        btnLoginFacebook.setOnClickListener { loginFacebook() }
+
+        btnFillDataSchool.setOnClickListener {
+            val intent = Intent(this,FillDataSchool::class.java)
+            startActivity(intent)
+        }
     }
 
     private fun registerUser(email: String, pass: String) {
@@ -145,7 +191,69 @@ class MainActivity : AppCompatActivity() {
 
         signInWithPhoneAuthCredential(credential)
     }
+    private fun loginGoogle() {
+        val signInIntent = googleSignInClient.signInIntent
+        startForResult.launch(signInIntent)
+    }
 
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        Log.d(TAG, "signInWithGoogle:credential generated")
+        signInWithCredential(credential)
+    }
+    private fun signInWithCredential(credential: AuthCredential) {
+        auth.currentUser!!.linkWithCredential(credential) // lo uso para loguearme con distintos proveedores en vez de signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithCredential:success")
+                    val user = task.result?.user  //auth.currentUser
+
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+
+                }
+            }
+            .addOnFailureListener {
+                Log.w(TAG, "signInWithCredential:fallo, pero estoy manejando el error")
+            }
+            .addOnSuccessListener {
+                Log.d(TAG, "signInWithCredential:se loggeo, pero estoy manejando el success")
+            }
+    }
+
+    private fun loginFacebook() {
+
+        LoginManager.getInstance().logInWithReadPermissions(this, listOf("email","public_profile"))
+
+        LoginManager.getInstance().registerCallback(callbackManager, object :
+            FacebookCallback<LoginResult> {
+            override fun onSuccess(loginResult: LoginResult) {
+                Log.d(TAG, "facebook:onSuccess:$loginResult")
+                handleFacebookAccessToken(loginResult.accessToken)
+            }
+
+            override fun onCancel() {
+                Log.d(TAG, "facebook:onCancel")
+            }
+
+            override fun onError(error: FacebookException) {
+                Log.d(TAG, "facebook:onError", error)
+            }
+        })
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+    }
+
+    private fun handleFacebookAccessToken(token: AccessToken) {
+        Log.d(TAG, "handleFacebookAccessToken:$token")
+
+        val credential = FacebookAuthProvider.getCredential(token.token)
+        signInWithCredential(credential)
+    }
     private fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential) {
         auth.signInWithCredential(credential)
             .addOnCompleteListener(this) { task ->
